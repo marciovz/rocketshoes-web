548fe3c128dbbc4ea711e596e29cb8d04c4683a2
marciovz <marciovz@yahoo.com.br>
2019-11-12 19:13:34 -0300

    Implementado a estilização global

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyle from './styles/global';
+
 import Routes from './routes';
 
 export default function App() {
@@ -8,6 +10,7 @@ export default function App() {
     <BrowserRouter>
       {/* <Header /> */}
       <Routes />
+      <GlobalStyle />
     </BrowserRouter>
   );
 }
