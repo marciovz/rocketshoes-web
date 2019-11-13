9eac4148e806bf4bbb4221b3d535aae5f0fc7db1
marciovz <marciovz@yahoo.com.br>
2019-11-12 22:24:54 -0300

    Implemantação da página Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,111 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-actual-masculino/20/D22-1319-120/D22-1319-120_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muiot barato</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-actual-masculino/20/D22-1319-120/D22-1319-120_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muiot barato</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-actual-masculino/20/D22-1319-120/D22-1319-120_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muiot barato</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-actual-masculino/20/D22-1319-120/D22-1319-120_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muiot barato</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-actual-masculino/20/D22-1319-120/D22-1319-120_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muiot barato</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-olympikus-actual-masculino/20/D22-1319-120/D22-1319-120_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muiot barato</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
